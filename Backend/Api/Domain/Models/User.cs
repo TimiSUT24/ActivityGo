6bfe1fd08172464783c1ad4f4cdcc74e4729a836
Tim <tim.petersenfalkenberg@outlook.com>
2025-09-30 09:34:29 +0200

    Update User.cs

--- Backend/Api/Domain/Models/User.cs
+++ Backend/Api/Domain/Models/User.cs
@@ -2,8 +2,6 @@ namespace Domain.Models;
 
 public class User : IdentityUser
 {
-    public string UserName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public string Role { get; set; } = "User";
+   public string Firstname {get; set;}
+   public string Lastname {get; set;}
 }
