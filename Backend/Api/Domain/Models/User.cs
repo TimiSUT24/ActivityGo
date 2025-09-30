334f14e72e51f09a2505f717938a139d011aac6f
alexanderek <Alexander.ek@live.se>
2025-09-30 09:11:47 +0200

    adde BaseEntity & User

---
+++ Backend/Api/Domain/Models/User.cs
@@ -0,0 +1,9 @@
+namespace Domain.Models;
+
+public class User : BaseEntity
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = "User";
+}
