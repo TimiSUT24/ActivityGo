8faaf89646f7f2311a13b2d1c003f65ce60746a2
Fredrik Jonsson <Fredrikandrejonson@hotmail.com>
2025-10-02 15:09:50 +0200

    Add activity folder structure and repository interface
    Updated `Api.csproj` to include a new folder reference for `Controllers\Admin\Activity\`.
    Revised `Application.csproj` to restructure DTOs for activities into separate `Response` and `Request` folders.
    Introduced `IActivityRepository` interface in `IActivityRepository.cs` with methods for checking activity existence and querying activities.

---
+++ Backend/Api/Domain/Interfaces/IActivityRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IActivityRepository : IGenericRepository<SportActivity>
+    {
+        Task<bool> ExistsByNameAsync(string name, CancellationToken cancellationToken = default);
+        IQueryable<SportActivity> Query();
+    }
+}
