8f6d796520bc073da4a415fd402ed0090e448651
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-10-07 20:56:57 +0200

    Add register in app.js

--- Frontend/react/src/App.jsx
+++ Frontend/react/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Link } from "react-router-dom";
-import LoginForm from "./Components/LoginForm"; 
+import LoginForm from "./Components/LoginForm";
+import RegisterPage from "./Pages/RegisterPage";
 
 function Home() {
   return <h1 style={{ padding: 16 }}>Välkommen</h1>;
@@ -11,10 +12,12 @@ export default function App() {
       <nav style={{ display: "flex", gap: 12, padding: 12 }}>
         <Link to="/">Hem</Link>
         <Link to="/login">Logga in</Link>
+        <Link to="/register">Register</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterPage />}></Route>
       </Routes>
     </>
   );
