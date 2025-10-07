4189c7aeedc6db823d11bb45477568fef6fde8d9
alexanderek <Alexander.ek@live.se>
2025-10-07 15:36:15 +0200

    Add authentication methods to IAuthService interface

--- Backend/Api/Application/Auth/Interface/IAuthService.cs
+++ Backend/Api/Application/Auth/Interface/IAuthService.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Application.Auth.DTO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Application.Auth.Interface
 {
     public interface IAuthService
     {
+        Task<(AuthResult? Result, string? RefreshToken)> LoginAsync(LoginDto dto, string? ipAddress, CancellationToken cancellationToken);
+        Task<(AuthResult? Result, string? RefreshToken)> RefreshAsync(string refreshToken, string? ipAddress, CancellationToken cancellationToken);
+        Task LogoutAsync(string userId, string? ipAddress, CancellationToken cancellationToken);
     }
 }
