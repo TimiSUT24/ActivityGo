3b12ebf27f8aa5d7b507a20d9647babe148aa5aa
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-10-07 15:34:17 +0200

    Added logic in RegisterForm
    Added inputs for email,password,firstname,lastname with validation for email and password

--- Frontend/react/src/Components/RegisterForm.jsx
+++ Frontend/react/src/Components/RegisterForm.jsx
@@ -6,13 +6,75 @@ export default function RegisterForm (){
     const [formData, setFormData] = useState({
         email:"",
         password:"",
+        firstname: "",
+        lastname: ""
     });
 
     const [message, setMessage] = useState("");
 
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value}) // updates state fromData when typing email for example 
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if(!formData.email || !formData.password){
+            setMessage("Email and password required")
+            return;
+        }
+
+    try{
+
+        await axios.post(`${import.meta.env.VITE_API_URL}/api/Auth/register`,formData);
+        
+        setMessage("Registration successful!")
+        }
+        catch(error)
+        {
+            setMessage(error.response?.data || "Registration failed")
+        }
+    }
+
     return (
         <div>
-            
+            <form onSubmit = {handleSubmit} className = "register-form">
+                <input 
+                type = "text" 
+                name = "email"
+                placeholder = "email"
+                value = {formData.email}
+                onChange = {handleChange}
+                />
+
+                <input 
+                type="text" 
+                name = "password"
+                placeholder = "password"
+                value = {formData.password}
+                onChange = {handleChange}
+                />
+
+                <input 
+                type="text" 
+                name = "firstname"
+                placeholder = "firstname"
+                value = {formData.firstname}
+                onChange = {handleChange}
+                />
+
+                <input 
+                type="text" 
+                name = "lastname"
+                placeholder = "lastname"
+                value = {formData.lastname}
+                onChange = {handleChange}
+                />
+
+                <button type = "submit">Register</button>
+
+                {message && <p>{message}</p>}
+            </form>
         </div>
     )
 }
