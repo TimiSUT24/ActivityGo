df278d4bb464567a9220f720dc9a7d402f6a3044
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-10-08 11:30:45 +0200

    Change to NavLink instead of Link
    gives more control in css if i want to change navbar in homepage, registerpage etc

--- Frontend/react/src/App.jsx
+++ Frontend/react/src/App.jsx
@@ -1,16 +1,23 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, NavLink, useLocation } from "react-router-dom";
 import LoginForm from "./Components/LoginForm";
 import RegisterPage from "./Pages/RegisterPage";
 import HomePage from "./Pages/HomePage";
 
 
 export default function App() {
+  const location = useLocation();
+
+  const pageClass = 
+        location.pathname === "/" ? "home-navbar" :
+        location.pathname === "/login" ? "login-navbar" : 
+        location.pathname === "/register" ? "register-navbar" : "default-navbar";
+
   return (
     <>
-      <nav className ="nav-bar"style={{ display: "flex", gap: 12, padding: 12 }}>
-        <Link to="/">Hem</Link>
-        <Link to="/login">Logga in</Link>
-        <Link to="/register">Register</Link>
+      <nav className ={`nav-bar ${pageClass}`}style={{ display: "flex", gap: 12, padding: 12 }}>
+        <NavLink to="/" className ="nav-link">Hem</NavLink>
+        <NavLink to="/login" className ="nav-link">Logga in</NavLink>
+        <NavLink to="/register" className ="nav-link">Register</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
