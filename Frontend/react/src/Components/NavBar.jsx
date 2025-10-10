be3329c9f8c4e5480ab152330615fd79b9bdb013
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-10-10 12:42:26 +0200

    Hide links
    Hide links/images in some pages

--- Frontend/react/src/Components/NavBar.jsx
+++ Frontend/react/src/Components/NavBar.jsx
@@ -24,6 +24,7 @@ export default function NavBar(){
     const pageClass = 
         location.pathname === "/" ? "home-navbar" :
         location.pathname === "/login" ? "login-navbar" : 
+        location.pathname === "/user" ? "user-navbar" :
         location.pathname === "/register" ? "register-navbar" : "default-navbar";
     return (
         <nav className ={`nav-bar ${pageClass}`}style={{padding: 12 }}>
@@ -31,24 +32,33 @@ export default function NavBar(){
                 <div className="nav-img">   
                     <img src="/IMG/Activigotitle.png" alt="" className="activigo-title"/>                 
                 </div>
-                <div className="nav-links">                 
+                <div className="nav-links">   
+
                 <NavLink to="/" className ="nav-link" id="nav1">
-                {location.pathname === '/' && ( //Show image only in homePage
+                {(location.pathname === '/' ||  location.pathname === '/user') && ( //Show image only in homePage/user
                 <img src="/IMG/Mario-Mushroom-Step-10.webp" alt="" height={20} width={20}/>
                 )}  Hem</NavLink>  
 
                 <NavLink to="/" className ="nav-link" id="nav2">
-                {location.pathname === '/' && ( 
+                {(location.pathname === '/' ||  location.pathname === '/user') && ( 
                 <img src="/IMG/icons8-pixel-star-48.png" alt="" height={20} width={20}/>
                 )}Sök Aktivitet</NavLink>
 
-                <NavLink to="/" className ="nav-link" id="nav3">
-                {location.pathname === '/' && ( 
-                <img src="/IMG/bookinicon.png" alt="" height={20} width={20}/>
-                )}Mina Bokningar</NavLink>
+                
+                {location.pathname === '/user' && ( 
+                <>
+                    <NavLink to="/user" className ="nav-link" id="nav3"><img src="/IMG/bookinicon.png" alt="" height={20} width={20}/>Mina Bokningar</NavLink>                  
+                </>
+                )}
 
-                <NavLink to="/login" className ="nav-link" id="nav4">Logga in</NavLink>
-                <NavLink to="/register" className ="nav-link" id="nav5">Registrera</NavLink>
+                <NavLink to="/user" className ="nav-link" id="nav6">Mina Sidor</NavLink>
+
+                {location.pathname !== "/user" &&(
+                <>
+                <NavLink to="/login" className ="nav-link" id="nav4">Logga in</NavLink> 
+                <NavLink to="/register" className ="nav-link" id="nav5">Registrera</NavLink> 
+                </>
+                )}                                                                
                 </div>   
             </div>              
         </nav>
