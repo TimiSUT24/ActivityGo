c52a42be8636c0c133c691bdf26de55b4aa0ab88
Viggo Kristensen <viggo.kristensen@hotmail.com>
2025-10-14 15:11:48 +0200

    Service for Category

---
+++ Backend/Api/Application/Category/Interface/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Category.DTO;
+
+namespace Application.Services.Interfaces;
+public interface ICategoryService
+{
+    Task<IReadOnlyList<CategoryReadDto>> GetAllAsync(CancellationToken ct = default);
+    Task<CategoryReadDto?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task<Guid> CreateAsync(CategoryCreateDto dto, CancellationToken ct = default);
+    Task<bool> UpdateAsync(Guid id, CategoryUpdateDto dto, CancellationToken ct = default);
+    Task<bool> DeleteAsync(Guid id, CancellationToken ct = default); // hårddelete
+    Task<bool> SoftDeactivateAsync(Guid id, CancellationToken ct = default); // sätter IsActive=false
+}
