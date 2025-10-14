0c2619d3b7fde9ad4a04badcddc83f2d588f494f
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-10-14 18:30:25 +0200

    Add globalexceptionmiddleware class

---
+++ Backend/Api/Api/Middleware/GlobalExceptionMiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace Api.Middleware
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionMiddleware> _logger;
+
+        public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unhandled exception occurred: {Message}", ex.Message);
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+    }
+}
