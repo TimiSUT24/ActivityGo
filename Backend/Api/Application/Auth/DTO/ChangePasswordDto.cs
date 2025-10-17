13db393b0599c64979030ce34925a50b874a1f5f
Viggo Kristensen <viggo.kristensen@hotmail.com>
2025-10-17 18:26:44 +0200

    Dtos to update user information

---
+++ Backend/Api/Application/Auth/DTO/ChangePasswordDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Auth.DTO
+{
+    public record ChangePasswordDto(
+        string CurrentPassword,
+        string NewPassword
+    );
+}
