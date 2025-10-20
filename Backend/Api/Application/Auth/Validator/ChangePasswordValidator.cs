5e561e4d8a92fa328aaeb3b3b6e6515e9ac40c60
Viggo Kristensen <viggo.kristensen@hotmail.com>
2025-10-20 11:31:27 +0200

    Validators

---
+++ Backend/Api/Application/Auth/Validator/ChangePasswordValidator.cs
@@ -0,0 +1,21 @@
+﻿using Application.Auth.DTO;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Auth.Validator
+{
+    public class ChangePasswordValidator : AbstractValidator<ChangePasswordDto>
+    {
+        public ChangePasswordValidator()
+        {
+            RuleFor(x => x.CurrentPassword).NotEmpty();
+            RuleFor(x => x.NewPassword)
+                .NotEmpty()
+                .MinimumLength(6);
+        }
+    }
+}
