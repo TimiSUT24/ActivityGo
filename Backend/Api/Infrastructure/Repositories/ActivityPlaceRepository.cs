8217940555420b3c41c45e69eb77211a2ca7ecec
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-10-21 17:20:42 +0200

    added validations in add method in acitivityoccurrencesService

---
+++ Backend/Api/Infrastructure/Repositories/ActivityPlaceRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+using Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ActivityPlaceRepository : GenericRepository<ActivityPlace>, IActivityPlaceRepository
+    {
+        private readonly AppDbContext _db;
+
+        public ActivityPlaceRepository(AppDbContext db) : base(db) => _db = db;
+   
+    }
+}
