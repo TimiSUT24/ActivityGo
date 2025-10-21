e6a1d9d54d6ab716f7b24b552db89f84f4716994
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-10-21 16:09:42 +0200

    Added activityplace model/made relations with place/sportactivity/added into dbcontext

---
+++ Backend/Api/Domain/Models/ActivityPlace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class ActivityPlace
+    {
+        public Guid SportActivityId { get; set; }
+        public SportActivity SportActivity { get; set; } = null!;
+
+        public Guid PlaceId { get; set; }
+        public Place Place { get; set; } = null!;
+    }
+}
